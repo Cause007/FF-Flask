827bed34148e3b6319daa43710999a8c4c0730c2
Evan Cosman <evancosman@gmail.com>
2024-07-02 15:05:23 -0700

    second commit

--- models.py
+++ models.py
@@ -3,8 +3,7 @@ from flask_migrate import Migrate
 import uuid
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from flask_login import LoginManager
+from flask_login import UserMixin, LoginManager
 from flask_marshmallow import Marshmallow
 import secrets
 
